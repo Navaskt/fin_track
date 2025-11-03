e2217126cb1ba6a6790ec789409c6febaad85ddb
Mohammed Navas <navaskt38@gmail.com>
2025-11-03 15:48:19 +0400

    Add iOS app icon to the project

--- lib/app/theme/app_theme.dart
+++ lib/app/theme/app_theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const _seed = Color(0xFF26C6DA); // pick your brand color
+const _seed = Color(0xFF1E88E5); // pick your brand color
 
 ThemeData buildLightTheme() {
   final colorScheme = ColorScheme.fromSeed(
